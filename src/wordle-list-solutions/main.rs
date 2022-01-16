ab5f62f2a8400af673154a34604751895bd2a560
Kelly Norton <kellegous@gmail.com>
2022-01-16 18:46:23 -0500

    .

---
+++ src/wordle-list-solutions/main.rs
@@ -0,0 +1,39 @@
+use chrono::prelude::*;
+use chrono::{Duration, NaiveDate};
+use std::error::Error;
+use std::fs;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn Error>> {
+	let matches = clap::App::new("wordle-list-solutions")
+		.arg(
+			clap::Arg::new("words")
+				.long("words")
+				.takes_value(true)
+				.default_value("words")
+				.help("list of words"),
+		)
+		.arg(
+			clap::Arg::new("n")
+				.long("n")
+				.takes_value(true)
+				.default_value("100")
+				.help("number of solutions"),
+		)
+		.get_matches();
+
+	let words = BufReader::new(fs::File::open(matches.value_of("words").unwrap())?)
+		.lines()
+		.collect::<Result<Vec<_>, _>>()?;
+	let n = matches.value_of("n").unwrap().parse::<usize>()?;
+
+	let start = NaiveDate::from_ymd(2021, 6, 19);
+	let today = Local::now().date().naive_local();
+	for i in 0..n {
+		let day = today + Duration::days(i as i64);
+		let num = day.signed_duration_since(start).num_days();
+		let word = words[num as usize % words.len()].to_uppercase();
+		println!("{}\t#{}\t{}", day, num, word);
+	}
+	Ok(())
+}
